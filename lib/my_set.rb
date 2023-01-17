6b61aa0077383e3bdd92a44f971868d1a65eb6c6
Parker Parham <parkerparham@gmail.com>
2023-01-17 18:01:08 -0500

    Done with assignment

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,4 +1,26 @@
 class MySet
-    def initialize
+    def initialize(enummerable = [])
+        @hash = {}
+        enummerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
     end
 end
